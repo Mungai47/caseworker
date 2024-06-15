bf50bca0012c2775fb783c8d790670018d09d36a
josephmburumungai99 <josephmburumungai99@gmail.com>
2024-06-15 20:49:44 +0000

    Installed Shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <p className = "text-3xl font-bold ">Home page</p>
-      <p className="text-xl">This is the home page</p>
-    </div>    
+    <div>
+      <p>This is the Home page</p>
+    </div>   
   );
 }
